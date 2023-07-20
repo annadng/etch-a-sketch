eb992a9ab2b1be4d070610b8d24d9c661d1990e4
annadng <annadang2001@gmail.com>
2023-07-20 23:20:22 +1000

    Add event listener to change color of grids

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const gridContainer = document.querySelector('.grid-squares-div');
 
 function createGrid(gridSize) {
     let gridArea = gridSize * gridSize;
-    for (i=0; i < gridArea; i++) {
-        const gridSquares = document.createElement('div');
-        gridSquares.classList.add('grid-squares');
-        gridContainer.appendChild(gridSquares);
+    for (let i=0; i < gridArea; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridContainer.appendChild(gridSquare);
+        gridSquare.style.backgroundColor = 'grey';
+        gridSquare.addEventListener('mousedown', colorPixels);
     }
     gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+}
+
+function colorPixels() {
+    this.style.backgroundColor = 'pink';
+    console.log('hello');
+}
+
+window.onload = () => {
+    createGrid(20);
 }
